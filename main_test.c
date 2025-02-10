92c163c82b902d51f1dd4a986186e86f2f045c6d
YellowGreenLime <nlime3141592@naver.com>
2025-02-11 03:23:23 +0900

    [Test] Test completed operation Create, Open, Close for B-Tree structure file

--- main_test.c
+++ main_test.c
@@ -1,3 +1,28 @@
+#include "bfile.h"
+
+int main_test_Create()
+{
+    CreateBFile("./bfile.dat");
+    printf("main_test_Create OK.\n");
+    return 0;
+}
+
+int main_test_OpenAndClose()
+{
+    BFileRuntime* bFileRuntime = OpenBFile("./bfile.dat");
+
+    printf("addrRootNode == %d\n", bFileRuntime->bFileHeader->addrRootNode);
+    printf("degree == %d\n", bFileRuntime->bFileHeader->degree);
+    printf("recordLength == %d\n", bFileRuntime->bFileHeader->recordLength);
+    printf("keyLength == %d\n", bFileRuntime->bFileHeader->keyLength);
+    printf("valueLength == %d\n", bFileRuntime->bFileHeader->valueLength);
+    printf("main_test_OpenAndClose OK.\n");
+
+    CloseBFile(bFileRuntime);
+
+    return 0;
+}
+
 int main_test(int argc, char* argv[])
 {
     // NOTE: 실행하고 싶은 테스트 함수의 인덱스를 기입하세요.
@@ -8,6 +33,8 @@ int main_test(int argc, char* argv[])
 
     // NOTE: 이 곳에 등록하고 싶은 함수의 함수 포인터를 등록합니다.
     int (*main_test_EntryPoints[10])();
+    main_test_EntryPoints[index++] = main_test_Create;
+    main_test_EntryPoints[index++] = main_test_OpenAndClose;
 
     return main_test_EntryPoints[mainIndex]();
 }
